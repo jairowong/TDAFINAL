3f458f4d3c251bac2e4d4bfec3c4cc3fa07b233d
jairowong <133531948+jairowong@users.noreply.github.com>
2025-01-07 16:32:20 -0500

    implementacion del servicio
    agregando servicio Matricula y DetalleMatricula

--- MSMatricula/MSMatricula/src/main/java/GestionMatricula/MSMatricula/service/IDetalleMatricula.java
+++ MSMatricula/MSMatricula/src/main/java/GestionMatricula/MSMatricula/service/IDetalleMatricula.java
@@ -6,42 +6,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import GestionMatricula.MSMatricula.model.DetalleMatricula;
+import GestionMatricula.MSMatricula.repositorio.DetalleMatriculaRepository;
 
 @Service
 public class IDetalleMatricula implements DetallaMatriculaIpm {
 
     @Autowired
+    DetalleMatriculaRepository detalleRepository;
     
 
     @Override
     public List<DetalleMatricula> mostrarDetalleMatricula() {
-
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrarDetalleMatricula'");
+        return (List<DetalleMatricula>) detalleRepository.findAll();
     }
 
     @Override
     public DetalleMatricula mostrarDetalleMatriId(Integer id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrarDetalleMatriId'");
+        return detalleRepository.findById(id).orElse(null);
     }
 
     @Override
     public DetalleMatricula crearDetalleMatri(DetalleMatricula detalle) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'crearDetalleMatri'");
+     return detalleRepository.save(detalle);
     }
 
     @Override
     public DetalleMatricula modificarDetalleMatri(DetalleMatricula detalle) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'modificarDetalleMatri'");
+     return detalleRepository.save(detalle);
     }
 
     @Override
     public boolean eliminarDetalleMatri(Integer id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'eliminarDetalleMatri'");
+        detalleRepository.existsById(id);
+        if (detalleRepository.existsById(id)) {
+            detalleRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+
     }
     
 }
