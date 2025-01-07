cdaee65af9c5439e3d0fec14433202664edc8ebc
YersonTineo56 <150179944+YersonTineo56@users.noreply.github.com>
2025-01-07 15:11:34 -0500

    Update AuthRequest.java
    Cambios en algunos codigos

--- MSUsuario/MSUsuario/src/main/java/GestionUsuario/MSUsuario/dto/AuthRequest.java
+++ MSUsuario/MSUsuario/src/main/java/GestionUsuario/MSUsuario/dto/AuthRequest.java
@@ -1,35 +1,33 @@
 package GestionUsuario.MSUsuario.dto;
 
 import java.io.Serializable;
- 
-public class AuthRequest implements Serializable {
 
+public class AuthRequest implements Serializable {
     private static final long serialVersionUID = 1L;
+
     private String username;
     private String password;
- 
-    public AuthRequest() {
- 
-    }
- 
+
+    public AuthRequest() {}
+
     public AuthRequest(String username, String password) {
-        this.setUsername(username);
-        this.setPassword(password);
-    }
- 
-    public String getPassword() {
-        return password;
-    }
- 
-    public void setPassword(String password) {
+        this.username = username;
         this.password = password;
     }
- 
+
     public String getUsername() {
         return username;
     }
- 
+
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
