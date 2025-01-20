51ef3dc947ef67fe38b3a919ac0ef38443ca24c8
jairowong <133531948+jairowong@users.noreply.github.com>
2025-01-20 01:12:13 -0500

    avanzando matricula
    controller

--- MSMatricula/MSMatricula/src/main/java/GestionMatricula/MSMatricula/controllers/Matricula.java
+++ MSMatricula/MSMatricula/src/main/java/GestionMatricula/MSMatricula/controllers/Matricula.java
@@ -1,5 +1,7 @@
 package GestionMatricula.MSMatricula.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,5 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class Matricula {
 
+    private static final Logger Logger= LoggerFactory.getLogger(Matricula.class);
     
 }
